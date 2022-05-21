5568baa2dd1f831a1b32f624a9d3447ea2e86b6d
Kyle Coleman <kcoleman20@cnm.edu>
2022-05-21 17:26:08 -0600

    Initial commit, practicing

--- Topics/Integer types and operations/Difference of times/src/Main.java
+++ Topics/Integer types and operations/Difference of times/src/Main.java
@@ -4,6 +4,11 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int inputOne;
+        int inputTwo;
+        int hourConversion;
+        hourConversion = ("" * 60) * 60;
+
         System.out.println(51511);
 
     }
